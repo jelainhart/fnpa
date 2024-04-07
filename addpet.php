1206db023077b1d5f8d96c914dd5a096a9281536
eshakhator <eshakhator@gmail.com>
2024-04-07 11:22:14 -0400

    Add styling

--- addpet.php
+++ addpet.php
@@ -28,11 +28,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')   // GET
   <meta name="keywords" content="Pet Finder, register pet">
   
   <title>Register Pet</title>
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">  
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">   
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+  <link rel="stylesheet" href="styles.css" /> 
 </head>
 
 <body> 
+  
+<?php include('header.html') ?>
+
 <div class="container">
   <div class="row g-3 mt-2">
     <div class="col">
