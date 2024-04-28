0e7c4793523b3ab9f70cdcf28df881f3ec4982f8
ran5tcw <123332198+ran5tcw@users.noreply.github.com>
2024-04-28 17:01:41 -0400

    shows recent sightings

--- db-home.php
+++ db-home.php
@@ -53,7 +53,7 @@ function getRecentSightings()
 {
    //reuse instance of db
    global $db;
-   $query = "SELECT species, breed, fur_color, fur_pattern, eye_color, pet_size, pet_condition, additional_description, street_name, zip_code, city, email_address, report_id FROM `reports` natural join pet_sightings natural join animal natural join lost_animal natural join people natural join found_reports ORDER BY date DESC LIMIT 3";
+   $query = "SELECT species, breed, fur_color, fur_pattern, eye_color, pet_size, pet_condition, additional_description, street_name, zip_code, city, email_address, report_id FROM `reports` natural join pet_sightings natural join animal natural join lost_animal natural join people natural join found_reports ORDER BY report_id DESC LIMIT 3";
    $statement = $db->prepare($query); //compile
    $statement->execute();
    $result = $statement->fetchAll(); //fetch() just retrieves only the first row
